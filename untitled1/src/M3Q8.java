dd2fd2021e6b1ed03c71a96b595474b7e8fdb07d
jiaoshen2000 <56907623+jiaoshen2000@users.noreply.github.com>
2019-12-04 15:54:21 +0800

    Add files via upload

---
+++ untitled1/src/M3Q8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class M3Q8 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int v1=0;
+        String str=scn.nextLine();
+        String strArr[]=str.split(" ");
+        for (int i=0;i<strArr.length;i++){
+            v1=Integer.parseInt(strArr[i]);
+            System.out.print(v1*v1+"\t");
+        }
+    }
+}
